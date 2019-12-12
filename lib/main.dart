974407e41653cad9c75dfcc7bd2ed22966bff73c
Xinsong Li <lixinso@gmail.com>
2019-12-12 02:57:09 +0000

    load more works; with issue

--- lib/main.dart
+++ lib/main.dart
@@ -44,16 +44,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
+  int present = 0;
+  int perPage = 15;
+
+  final originalItems = List<String>.generate(10000, (i) => "Item $i");
+  var items = List<String>();
+
+  @override
+  void initState(){
+    super.initState();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      items.addAll(originalItems.getRange(present, present + perPage));
+      present = present + perPage;
     });
   }
 
@@ -71,41 +74,30 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: ListView.builder(
+          itemCount: (present <= originalItems.length)? items.length + 1: items.length,
+          itemBuilder: (context, index){
+            return (index == items.length) ?
+                Container(
+                  color: Colors.greenAccent,
+                  child: FlatButton(
+                    child: Text("load more"),
+                    onPressed: (){
+                      setState(() {
+                        if((present + perPage) > originalItems.length){
+                          items.addAll(originalItems.getRange(present, originalItems.length));
+                        }else{
+                          items.addAll(originalItems.getRange(present, present + perPage));
+                        }
+                        present = present + perPage;
+                      });
+                    },
+                  ),
+                )
+                : ListTile(
+                    title: Text('$items[index]'),
+                  );
+          }),
     );
   }
 }
